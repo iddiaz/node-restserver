1b3a8b40ae332eec4663d9ed55d90899a65f0d7b
iddiaz <correo@ivandiazdiaz.com>
2022-01-17 16:25:15 +0100

    configuración de sistema de busqueda en server

---
+++ controllers/buscar.js
@@ -0,0 +1,14 @@
+const { response } = require("express");
+
+const buscar = (req, res = response) =>{
+
+   res.json({
+      msg: 'Buscar...'
+   })
+
+}
+
+
+module.exports = {
+   buscar
+}
--- models/server.js
+++ models/server.js
@@ -10,6 +10,7 @@ class Server {
 
       this.paths = {
          auth:       '/api/auth',
+         buscar:       '/api/buscar',
          usuarios:   '/api/users',
          categorias: '/api/categorias',
          productos: '/api/productos'
@@ -44,6 +45,7 @@ class Server {
 
    routes(){
       this.app.use( this.paths.auth, require('../routes/auth'));
+      this.app.use( this.paths.buscar, require('../routes/buscar'));
       this.app.use( this.paths.categorias, require('../routes/categorias'));
       this.app.use( this.paths.usuarios , require('../routes/users.routes'));
       this.app.use( this.paths.productos , require('../routes/productos'));
