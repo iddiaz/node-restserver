3000f81da21185c4dac7a7b48d0c02728e258b7b
iddiaz <correo@ivandiazdiaz.com>
2022-01-18 18:53:28 +0100

    Borrar imagenes actualziadas en cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -172,7 +172,13 @@ const actualizarImagenCloudinary = async(req, res = response ) => {
 
    if( modelo.img ){
       
-      //todo...
+      const nombreArr = modelo.img.split('/');
+      const nombre = nombreArr[ nombreArr.length - 1]
+      const [ public_id ] = nombre.split('.');
+
+      cloudinary.uploader.destroy( public_id);
+
+      
    }
 
    const { tempFilePath } = req.files.archivo;
